65c7f03aba4adedb17fbd8553897eeacb78ef28a
Roy Didanie <roydidanie@gmail.com>
2025-09-24 13:53:40 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -66,10 +66,8 @@ export default function HomePage() {
         {/* Hero Content */}
         <div className="flex items-center max-lg:items-start max-lg:gap-10 justify-between px-24 max-lg:px-4 max-lg:flex-col mt-20 max-lg:mt-8 max-lg:w-full">
           <div className="flex-1 max-lg:flex-none max-lg:w-full max-lg:mb-16 max-md:mb-5">
-            <h1 className="text-5xl font-bold text-white leading-[50px] max-lg:text-left max-lg:text-5xl max-lg:w-full">
-              Scalable <br /> and secure <span className="text-[#2fa7df] italic">Software</span> solutions
-              <br />
-              
+            <h1 className="text-5xl font-bold text-white leading-[55px] max-lg:text-left max-lg:text-5xl max-lg:w-full">
+              Scalable <br /> and secure <br /> <span className="text-[#2fa7df] italic">Software</span> Solutions
             </h1>
           </div>
 
@@ -163,7 +161,7 @@ export default function HomePage() {
               </div> */}
               <div className="">
                 {/* <h3 className="text-xl font-bold mb-2 max-lg:leading-tight">For Consumers</h3> */}
-                <p className="text-lg">Transforming businesses with smart, industry-specific, and Ai-powered software solution that are designed to streamline operations, enhance productivity, and drive measurable growth.</p>
+                <p className="text-[clamp(0.5rem,1.5vw,2rem)]">Transforming businesses with smart, industry-specific, and Ai-powered software solution that are designed to streamline operations, enhance productivity, and drive measurable growth.</p>
               </div>
             </div>
 
@@ -196,7 +194,7 @@ export default function HomePage() {
               </div> */}
               <div className="">
                 {/* <h3 className="text-xl font-bold mb-2 max-lg:leading-tight">For Business Owners</h3> */}
-                <p className="text-lg">
+                <p className="text-[clamp(0.5rem,1.5vw,2rem)]">
                   We deliver dependable software foundations for industries that never stop moving - from HR to Retail, Accounting to Education, Property Management to Hospitality, and beyond.
                 </p>
               </div>
@@ -207,35 +205,31 @@ export default function HomePage() {
 
       {/* Newsletter Section */}
       <section className="py-12 max-lg:px-4 px-24 max-lg:py-12 bg-gray-100">
-        <div className="w-full mx-auto text-left flex items-center justify-between max-lg:flex-col max-lg:gap-6">
-          <div>
-            <h2 className="text-3xl  max-lg:text-2xl font-bold text-gray-900 mb-2">
-              Interested to know more ?
-            </h2>
-            <p className="text-gray-600 mb-0">
-             {`Share your email address and you'll be the first to know`}
-            </p>
-          </div>
+        <div className="w-full flex justify-between max-md:flex-col max-md:space-y-8">
+          <div className="flex flex-col space-y-4">
+            <div className="">
+              <h2 className="text-3xl  max-lg:text-2xl font-bold text-gray-900 mb-2">
+                Interested to know more ?
+              </h2>
+              <p className="text-gray-600 mb-0">
+              {`Share your email address and you'll be the first to know`}
+              </p>
+            </div>
 
-          <div className="flex items-center gap-4 max-lg:w-full">
-            <input
-              type="email"
-              placeholder="Your Email"
-              className="w-[400px] max-lg:w-full px-4 py-3 border border-gray-300 rounded-lg"
-            />
-            <button className="px-8 py-3 bg-gray-800 text-white rounded-lg hover:bg-gray-700">
-              SUBMIT
-            </button>
+            <div className="flex items-center gap-4 max-lg:w-full">
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="w-[400px] max-lg:w-full px-4 py-3 border border-gray-300 rounded-lg"
+              />
+              <button className="px-8 py-3 bg-gray-800 text-white rounded-lg hover:bg-gray-700">
+                SUBMIT
+              </button>
+            </div>
           </div>
-        </div>
-      </section>
-
-      {/* Footer */}
-      <footer className="bg-slate-900 py-6 px-24 max-lg:px-4">
-        <div className="flex flex-col flex-row max-md:flex-col max-md:w-full max-md:items-start max-md:gap-8 justify-between items-end text-white text-sm">
-           <div className="">
-            <p className="font-bold">Contact Us:</p>
-            <div className="flex flex-col space-y-3 mt-3">
+          <div className="">
+            <p className="font-bold text-2xl">Contact Us:</p>
+            <div className="flex flex-col space-y-3 mt-2">
               <div className="flex items-center space-x-2">
                 <Icon icon="hugeicons:mail-01" width="20" height="20" />
                 <p className="font-normal">info@baisoftglobal.com</p>
@@ -250,6 +244,13 @@ export default function HomePage() {
               </div>
             </div>
           </div>
+        </div>
+      </section>
+
+      {/* Footer */}
+      <footer className="bg-slate-900 py-6 px-24 max-lg:px-4">
+        <div className="flex flex-col flex-row max-md:flex-col max-md:w-full max-md:items-start max-md:gap-8 justify-between items-end text-white text-sm">
+           
           <div className="max-lg:text-center"> Copyright © {new Date().getFullYear()} Baisoft Global LTD. All Rights Reserved.</div>
          
           {/* <div className="flex items-center space-x-6 mt-4 sm:mt-0">
